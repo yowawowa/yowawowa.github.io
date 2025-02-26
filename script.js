5b26548427c664696bf75324e61966db6aa7c862
yowawowa <wladizzlaw@gmail.com>
2025-02-26 09:47:59 +0500

    fixes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const copyBtn = document.getElementById("copy-btn");
 const popup = document.getElementById("popup");
 
 copyBtn.addEventListener("click", () => {
-  const textToCopy = "irm https://get.activated.win | iex"; // replace with the text you want to copy
+  const textToCopy = "irm https://get.activated.win | iex";
   navigator.clipboard.writeText(textToCopy).then(() => {
     console.log("Text copied to clipboard");
     popup.classList.add("show");
