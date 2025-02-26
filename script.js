c1722f8ee986b595d2cfcb38fac483118f79e11e
yowawowa <wladizzlaw@gmail.com>
2025-02-26 10:21:54 +0500

    7+4

--- script.js
+++ script.js
@@ -27,9 +27,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // 2. Создаем конус
     // const geometry = new THREE.ConeGeometry(2, 4, 32);
-    // const geometry = new THREE.IcosahedronGeometry(2); // Икосаэдр
+    const geometry = new THREE.IcosahedronGeometry(7); // Икосаэдр
     // const geometry = new THREE.TorusGeometry(2, 0.6, 16, 100); 
-    const geometry = new THREE.OctahedronGeometry(7); // Октаэдр
+    const geometry_octa = new THREE.OctahedronGeometry(4); // Октаэдр
     
     // const geometry = new THREE.IcosahedronGeometry(2, 2);
 
@@ -45,8 +45,14 @@ document.addEventListener("DOMContentLoaded", function () {
       color: 0xff5733,
       wireframe: true,
     }); // Оранжевый каркас
+    const material_second = new THREE.MeshBasicMaterial({
+      color: 0x12c6bd,
+      wireframe: true,
+    }); // Оранжевый каркас
     const figure = new THREE.Mesh(geometry, material);
+    const figure_small = new THREE.Mesh(geometry_octa, material_second);
     scene.add(figure);
+    scene.add(figure_small);
 
     // 3. Позиционируем камеру
     camera.position.z = 6;
@@ -57,6 +63,9 @@ document.addEventListener("DOMContentLoaded", function () {
       figure.rotation.x += 0.01; // Вращение по X
       figure.rotation.y += 0.02; // Вращение по Y
       figure.rotation.z += 0.015; // Вращение по Z
+      figure_small.rotation.x -= 0.01; // Вращение по X
+      figure_small.rotation.y -= 0.02; // Вращение по Y
+      figure_small.rotation.z -= 0.015; // Вращение по Z
       renderer.render(scene, camera);
     }
 
